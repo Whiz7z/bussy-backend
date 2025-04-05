4b6b92adde80dbf353617f2d4037d3efae6782f8
Yevstratov Yevstratov <yevhen.y@thecomity.com>
2025-04-05 22:27:03 +0100

    cookies sending fix2

--- api/index.js
+++ api/index.js
@@ -11,6 +11,8 @@ require('../src/config/passport');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set('trust proxy', 1);
+
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
