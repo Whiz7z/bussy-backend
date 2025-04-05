94ac66ec0989abc36bece3fff72acbc765f08e26
Yevstratov Yevstratov <yevhen.y@thecomity.com>
2025-04-05 22:03:06 +0100

    google callback url fix

--- src/config/passport.js
+++ src/config/passport.js
@@ -17,12 +17,14 @@ passport.deserializeUser(async(user, done) => {
   }
 });
 
+const callbackURL = process.env.GOOGLE_CALLBACK_URL || '/api/auth/google/callback';
+
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: '/api/auth/google/callback',
+      callbackURL: callbackURL,
       scope: ['profile', 'email']
     },
     async (accessToken, refreshToken, profile, done) => {
